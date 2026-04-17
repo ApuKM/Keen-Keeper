082981f67ae530b457befc5cf9c350fecf626085
ApuKM <apukumar180@gmail.com>
2026-04-17 18:34:51 +0600

    data not cached for dynamic rendering

--- src/lib/friendsPromise.js
+++ src/lib/friendsPromise.js
@@ -1,6 +1,8 @@
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const friendsPromise = async () => {
-  const res = await fetch(`${baseUrl}/data.json`);
+  const res = await fetch(`${baseUrl}/data.json`, {
+    cache: "no-store",
+  });
   return res.json();
 };
